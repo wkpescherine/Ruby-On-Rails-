6d7e42c4388d6d15e8751a8108dba669148b458a
wkpescherine <wkpescherine@yahoo.com>
2019-09-11 00:11:08 -0400

    Add files via upload
    Completed Most of the character building attributes needed. As well as added so logic for other features
    
    ADDED:
    -Created the last pieces for the profession area 
    -Added attributes required  for stats
    -Added a new Start screen that allows for creating new chars or seeing list of previously created char
    
    FOCUS:
    -Now need to focus on gettong my POST and Get request working

--- asn/index.js
+++ asn/index.js
@@ -1,16 +1,54 @@
 const mainPanel = document.getElementById("main_area")
 const statsPanel = document.getElementById("stats_area")
+const playPanelStart = document.getElementById("play_area")
 const playPanel = document.getElementById("play_area")
 const messagePanel = document.getElementById("message_panel")
 
 playPanel.addEventListener("click",createChar)
+playPanelStart.addEventListener("click", startChoices)
 
-var stats = [0,0,0,0,0,0,0,0,0,0]
+var stats = [0,0,0,0,0,0,0,0]
+var tempStyle = [0,0,0,0]
+var tempRace = [0,0,0,0]
+var tempProf = [0,0,0,0]
 
 document.addEventListener('DOMContentLoaded', function(){
-	let nameInput= "<input style='margin:12px;' type='text' name='Char Name' >"
+	startScreen()
+}, false);
+
+//CHOICES SCREEN START
+//This is the screen when you first load up the page
+function startScreen(){
+	let listChar = "<h1 id='list'>Click Here To List All Characters</h1>"
+	playPanelStart.innerHTML += listChar
+	let buildNewChar = "<h1 id='build'>Click Here To Build A New Character</h1>"
+	playPanelStart.innerHTML += buildNewChar
+}
+
+function startChoices(e){
+	let choices = e.target.id
+
+	switch(choices){
+		case "build":
+			playPanel.innerHTML = " "
+			buildChar()
+			break;
+		case "list":
+			playPanel.innerHTML = " "
+			displayList()
+			break;
+	}
+}
+//CHOICES SCREEN END
+
+//CHAR CREATE START
+//This builds the mechanics needed for me to create a new char
+function buildChar(){
+	let nameInput= "<input id='inputName' style='margin:12px;' type='text' name='Char Name' >"
 	playPanel.innerHTML += nameInput
-	let submitInput= "<button style='margin:12px;'>SUBMIT</button>"
+	let nameSetButton= "<button id='name' style='margin:12px;'>Name Character</button>"
+	playPanel.innerHTML += nameSetButton
+	let submitInput= "<button id='submit' style='margin:12px;'>SUBMIT</button>"
 	playPanel.innerHTML += submitInput
 	let styleText = "<h4 style='margin: 16px;'> Select Play Style </h4>"
 	playPanel.innerHTML += styleText	
@@ -30,16 +68,15 @@ document.addEventListener('DOMContentLoaded', function(){
 	playPanel.innerHTML += btn3	
 	let classText = "<h4 style='margin: 16px;'> Select Class </h4>"
 	playPanel.innerHTML += classText
-	let btn4 = "<button style='font-size: 18px; margin: 18px;'> Warrior </button>"
+	let btn4 = "<button id='warrior' style='font-size: 18px; margin: 18px;'> Warrior </button>"
 	playPanel.innerHTML += btn4	
-	let btn5 = "<button style='font-size: 18px; margin: 18px;'> Mage </button>"
+	let btn5 = "<button id='mage' style='font-size: 18px; margin: 18px;'> Mage </button>"
 	playPanel.innerHTML += btn5
-	let btn6 = "<button style='font-size: 18px; margin: 18px;'> Assassin </button>"
+	let btn6 = "<button id='assassin' style='font-size: 18px; margin: 18px;'> Assassin </button>"
 	playPanel.innerHTML += btn6	
 
 	setStats()
-}, false);
-
+}
 
 function setStats(){
 	statsPanel.innerHTML =" "
@@ -49,37 +86,94 @@ function setStats(){
 	statsPanel.innerHTML += prof
 	let str = `Strength: ${stats[4]} <br>`
 	statsPanel.innerHTML += str
+	let dex = `Dexterity: ${stats[5]} <br>`
+	statsPanel.innerHTML += dex
+	let fort = `Fortitude: ${stats[6]} <br>`
+	statsPanel.innerHTML += fort
+	let wis = `Wisdom: ${stats[7]} <br>`
+	statsPanel.innerHTML += wis
 }
 
 function createChar(e){
 	let cond = e.target.id
 
 	switch(cond){
+		case "submit":
+			let total = stats[4]+stats[5]+stats[6]+stats[7]
+			if(stats[0]!=0 && total == 15){
+				console.log("posted to db")
+				//postToDB()
+			}else{
+				alert("You appear to be missing something")
+			} 	
+			break;
+		case "name":
+			stats[0] = document.getElementById("inputName").value
+			setMetrics()
+			break;
 		case "wise":
 			stats[1] = "Wise"
-			setStats()
+			tempStyle = [0,1,1,3]
+			setMetrics()
 			break;
 		case "good":
 			stats[1] = "Good"
-			setStats()
+			tempStyle = [1,1,2,1]
+			setMetrics()
 			break;
 		case "insane":
 			stats[1] = "Insane"
-			setStats()
+			tempStyle = [3,1,1,0]
+			setMetrics()
 			break;
 		case "orc":
 			stats[2] = " Orc"
-			setStats()
+			tempRace = [3,1,1,0]
+			setMetrics()
 			break;
 		case "human":
 			stats[2] = "Human"
-			setStats()
+			tempRace = [1,1,2,1]
+			setMetrics()
 			break;
 		case "dwarf":
 			stats[2] = "Dwarf"
-			setStats()
+			tempRace = [2,0,3,0]
+			setMetrics()
 			break;
-
+		case "warrior":
+			stats[3] = "Warrior"
+			tempProf = [4,0,1,0]
+			setMetrics()
+			break;
+		case "mage":
+			stats[3] = "Mage"
+			tempProf = [0,2,0,3]
+			setMetrics()
+			break;
+		case "assassin":
+			stats[3] = "Assassin"
+			tempProf = [2,4,0,0]
+			setMetrics()
+			break;		
 	}
 
+	function setMetrics(){
+		combStats()
+		setStats()
+	}
+
+	function combStats(){
+		stats[4] = tempStyle[0]+tempRace[0]+tempProf[0]
+		stats[5] = tempStyle[1]+tempRace[1]+tempProf[1]
+		stats[6] = tempStyle[2]+tempRace[2]+tempProf[2]
+		stats[7] = tempStyle[3]+tempRace[3]+tempProf[3]
+	}
 }
+//CHAR CREATE END
+
+//DISPLAY CHARS LIST START
+function displayList(){
+	playPanel.innerHTML = " "
+}
+//DISPLAY CHARS LIST END
